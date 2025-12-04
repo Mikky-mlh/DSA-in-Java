eada161b7b0d838238384e54b4f2a67fceea5957
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2025-12-04 22:00:13 +0530

    update

---
+++ Basic Maths/Count_Digits.java
@@ -0,0 +1,15 @@
+//TODO: Program to count digits
+import java.util.Scanner;
+public class Count_Digits {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int count = 0;
+        while (n > 0) {
+            n = n / 10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
