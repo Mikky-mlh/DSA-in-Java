f02dd159af0300991270594fe57118c4980e7a36
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2025-12-25 18:58:11 +0530

    Unique Char OP

--- Basic Maths/Unique_char_O.java
+++ Basic Maths/Unique_char_O.java
@@ -1,14 +1,20 @@
 public class Unique_char_O {
     public int firstUniqChar(String str) {
-    int[] occ= new int[199];
-        for(int i=0; i < str.length(); i++){
+        //! Find index of first non-repeating character in O(n) time
+        //! Use frequency array to count occurrences
+
+        int[] occ = new int[128]; // ASCII characters (0-127)
+        
+        for(int i = 0; i < str.length(); i++){ // Count frequency of each char
             occ[str.charAt(i)]++;
         }
-        for(int i=0;i<str.length();i++){
-            if(occ[str.charAt(i)]==1){
-                return i;
+        
+        for(int i = 0; i < str.length(); i++){ // Find first char with frequency 1
+            if(occ[str.charAt(i)] == 1){
+                return i; // Return index
             }
         }
-        return -1;
+        
+        return -1; // No unique character found
     }
 }
