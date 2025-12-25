e74ad03947fdde506ff59b84efded25084cbc186
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2025-12-25 18:57:55 +0530

    Unique Char

--- Basic Maths/Unique_char.java
+++ Basic Maths/Unique_char.java
@@ -1,5 +1,7 @@
-public class First_unique_Char_in_Str {
+public class Unique_char {
     public int firstUniqChar(String str) {
+        //* str.indexOf(character) gives the first index of character
+        //* str.lastIndexOf(character) gives the last index of character
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
             if (str.indexOf(c) == str.lastIndexOf(c)) {
