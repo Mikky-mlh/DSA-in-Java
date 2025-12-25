131352c447e3e0d050bf8f1ae03726a1a76c2a87
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2025-12-25 19:00:34 +0530

    Pivot

--- Basic Maths/PivotInt.java
+++ Basic Maths/PivotInt.java
@@ -1,14 +1,17 @@
 public class PivotInt {
     public int pivotInteger(int n) {
-
-        int totalSum = n * (n + 1) / 2;
+        //! Find pivot x where: sum(1 to x) = sum(x to n)
+        //* Formula: x(x+1)/2 = totalSum - x(x-1)/2
+        // Simplifies to: x² = totalSum
         
-        int x = (int) Math.sqrt(totalSum);
+        int totalSum = n * (n + 1) / 2; // Sum of 1 to n
         
-        if (x * x == totalSum) {
+        int x = (int) Math.sqrt(totalSum); // Check if totalSum is a perfect square
+        
+        if (x * x == totalSum) { // If yes, x is the pivot!
             return x;
         }
         
-        return -1;
+        return -1; // No pivot exists
     }
 }
