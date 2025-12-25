9eb05dc469d9f3a16833d2f1f100a7f8bcbdd521
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2025-12-25 18:58:51 +0530

    Count Prime

--- Basic Maths/Count_Prime.java
+++ Basic Maths/Count_Prime.java
@@ -1,20 +1,23 @@
 class Count_Prime {
     public int countPrimes(int n) {
-    if (n <= 2) return 0;
+        //! Sieve of Eratosthenes - Count primes less than n
+        
+        if (n <= 2) return 0; // No primes below 2
 
-    boolean[] isNotPrime = new boolean[n];
-    int count = 0;
-
-    for (int i = 2; i < n; i++) {
-        if (!isNotPrime[i]) {
-            count++;
-            if ((long)i * i < n) {
-                for (int j = i * i; j < n; j += i) {
-                    isNotPrime[j] = true;
+        boolean[] isNotPrime = new boolean[n]; // false = prime, true = composite
+        //& you can try with isPrime but then you will need to assign value o true to each index
+        int count = 0;
+        
+        for (int i = 2; i < n; i++) {
+            if (!isNotPrime[i]) { // Found a prime!
+                count++;
+                if ((long)i * i < n) { // Optimization: only mark if i² < n
+                    for (int j = i * i; j < n; j += i) { // Mark all multiples as composite
+                        isNotPrime[j] = true;
+                    }
                 }
             }
         }
+        return count;
     }
-    return count;
-}
 }
