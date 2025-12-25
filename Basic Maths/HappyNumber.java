c4b3a4665b5c62a81cc201482014f89cd70f9bde
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2025-12-25 19:00:06 +0530

    happy Number

--- Basic Maths/HappyNumber.java
+++ Basic Maths/HappyNumber.java
@@ -1,21 +1,21 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class HappyNumber {
+    //! Happy number are numbers that eventually reach 1 when iterated through a sequence of sum of squares of digits
     public boolean isHappy(int n) {
         Set<Integer> seen = new HashSet<>();
 
         while (n != 1 && !seen.contains(n)) {
-        seen.add(n);
-        int sum = 0;
+            seen.add(n);
+            int sum = 0;
 
-        while (n > 0) {
-            int digit = n%10; 
-            sum += digit*digit;
-            n /= 10; 
-        }
-    
-        n = sum; 
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+
+            n = sum;
         }
 
         return n == 1;
