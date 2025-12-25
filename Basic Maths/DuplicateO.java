1945b04908a64cf8fc853ee22955c1e545c4671b
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2025-12-25 18:59:19 +0530

    Diplicate OP

--- Basic Maths/DuplicateO.java
+++ Basic Maths/DuplicateO.java
@@ -2,7 +2,7 @@ import java.util.HashSet;
 //! Good optimization
 public class DuplicateO {
     public boolean containsDuplicate(int[] nums) {
-        // 📦 Track seen numbers
+        // Track seen numbers
         HashSet<Integer> set = new HashSet<>();
         for(int num:nums){
             // Check if the number 'num' is already in the 'set' HashSet.
