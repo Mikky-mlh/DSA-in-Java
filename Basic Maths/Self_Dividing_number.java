08b8165eba3e9d8db215a7146a21c73e25489202
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2025-12-25 19:01:07 +0530

    Seld Dividing numbers

--- Basic Maths/Self_Dividing_number.java
+++ Basic Maths/Self_Dividing_number.java
@@ -1,29 +1,32 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Self_Dividing_number {
     public List<Integer> selfDividingNumbers(int left, int right) {
+        //! Find all self-dividing numbers in range [left, right]
+        //! Self-dividing: divisible by each of its digits (e.g., 128: 128%1=0, 128%2=0, 128%8=0)
+        
         List<Integer> result = new ArrayList<>();
         
         for (int i = left; i <= right; i++) {
             if (isSelfDividing(i)) {
-                result.add(i); 
+                result.add(i);
             }
         }
-        return result; 
+        return result;
     }
 
     
     private boolean isSelfDividing(int i) {
         int temp = i;
+        
         while (temp > 0) {
-            int digit = temp % 10;
+            int digit = temp % 10; // Extract last digit
             
-            if (digit == 0 || i % digit != 0) {
-                return false; 
+            if (digit == 0 || i % digit != 0) { // Contains 0 or not divisible
+                return false;
             }
-            temp /= 10;
+            temp /= 10; // Remove last digit
         }
-        return true; 
+        return true;
     }
 }
