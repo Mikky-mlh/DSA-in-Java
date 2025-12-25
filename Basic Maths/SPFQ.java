d19adcd97b58abed7c0cb39b6f510829bf01540f
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2025-12-25 19:01:28 +0530

    SPF by queries

--- Basic Maths/SPFQ.java
+++ Basic Maths/SPFQ.java
@@ -1,30 +1,40 @@
 import java.util.*;
 
 public class SPFQ {
+    //! SPF = Smallest Prime Factor for each number
+    //! Precompute SPF using sieve, then factorize in O(log n)
+    
     static int[] spf;
 
     static void sieve(int MAX) {
         spf = new int[MAX + 1];
-        for (int i = 2; i <= MAX; i++) spf[i] = i;
+        
+        for (int i = 2; i <= MAX; i++) spf[i] = i; // Initialize: each number is its own SPF
+        
         for (int i = 2; i * i <= MAX; i++) {
-            if (spf[i] == i) {
-                for (int j = i * i; j <= MAX; j += i) {
-                    if (spf[j] == j) spf[j] = i;
+            if (spf[i] == i) { // i is prime
+                for (int j = i * i; j <= MAX; j += i) { // Mark multiples
+                    if (spf[j] == j) spf[j] = i; // Set SPF to smallest prime
                 }
             }
         }
     }
 
     static List<Integer> getFactorization(int n) {
+        //! Get prime factorization by repeatedly dividing by SPF
+        
         List<Integer> factors = new ArrayList<>();
+        
         while (n != 1) {
-            factors.add(spf[n]);
-            n /= spf[n];
+            factors.add(spf[n]); // Add smallest prime factor
+            n /= spf[n]; // Divide by SPF
         }
+        
         return factors;
     }
 
     public static void main(String[] args) {
+        //TESTCASES
         sieve(1000);
         System.out.println(getFactorization(120));
         System.out.println(getFactorization(37));
