2bf136981f4f4cff6a7863ecddca2c9d10929a3c
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2025-12-25 18:58:43 +0530

    Armstrong

--- Basic Maths/Armstrong_Number.java
+++ Basic Maths/Armstrong_Number.java
@@ -1,10 +1,11 @@
 public class Armstrong_Number {
     static boolean armstrongNumber(int n) {
+        //! Armstrong number are numbers that are equal to the sum of cubes of their digits
         int sum = 0;
         int temp = n;
         while(n>0){
             int digit = n%10;
-            sum = sum+(digit*digit*digit);
+            sum = sum+(digit*digit*digit); 
             n=n/10;
         }
         return sum==temp;
