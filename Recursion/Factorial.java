3b0be684b7cedcabc927b2ec582610876172f510
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2026-01-04 18:04:27 +0530

    factorial base change

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -10,7 +10,7 @@ public class Factorial {
         sc.close();
     }
     public static int fact(int n){
-        if(n==1 || n==0){
+        if(n==0){
             return 1;
         }
         return n*fact(n-1);
