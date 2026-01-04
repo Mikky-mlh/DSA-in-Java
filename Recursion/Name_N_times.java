44da88e0f74475d1e2642fd85ba720e557f58cfe
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2026-01-04 17:00:48 +0530

    problem

--- Recursion/Name_N_times.java
+++ Recursion/Name_N_times.java
@@ -1,5 +1,21 @@
-package JAVA.DSA-in-Java.Recursion;
-
+import java.util.*;
 public class Name_N_times {
-    
+
+    public static void print(String name, int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(name);
+        print(name, n-1);
+    }
+
+    public static void main(String[] args){
+        int n;
+        System.out.print("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        print("Yuvraj", n);
+        sc.close();
+    }
 }
+
