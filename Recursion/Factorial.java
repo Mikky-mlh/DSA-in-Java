d96a20a760dd6b5c431f8c7f57249262be551084
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2026-01-04 18:03:39 +0530

    factorial

---
+++ Recursion/Factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int n;
+        System.out.print("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        System.out.println(fact(n));
+        sc.close();
+    }
+    public static int fact(int n){
+        if(n==1 || n==0){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+}
