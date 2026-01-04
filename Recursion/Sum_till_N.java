55554b5c1891d6a28eba3e562f07220c9370a44d
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2026-01-04 18:20:32 +0530

    both rec

--- Recursion/Sum_till_N.java
+++ Recursion/Sum_till_N.java
@@ -1,10 +1,20 @@
 import java.util.Scanner;
 public class Sum_till_N {
     public static int printSum(int n){
+        //* Functional Recursion
         if(n==0){
             return 0;
         }
         return n + printSum(n-1);
+    }
+
+    public static void printSumParam(int n, int sum){
+        //* Parameterised Recursion
+        if(n==0){
+            System.out.println(sum);
+            return;
+        }
+        printSumParam(n-1, sum+n);
     }
 
     public static void main(String[] args){
@@ -13,6 +23,7 @@ public class Sum_till_N {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         System.out.println(printSum(n));
+        printSumParam(n, 0);
         sc.close();
     }
 }
