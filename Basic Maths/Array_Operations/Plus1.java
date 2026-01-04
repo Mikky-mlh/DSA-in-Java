06e4e834de0733242e2a2f72fc47d8a0c5cba8e8
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2026-01-04 17:00:20 +0530

    plus one error solved

--- Basic Maths/Array_Operations/Plus1.java
+++ Basic Maths/Array_Operations/Plus1.java
@@ -1,7 +1,7 @@
 public class Plus1 {
     public int[] plusOne(int[] digits) {
         //! Add 1 to a number represented as an array of digits
-        // Example: [1,2,3] + 1 = [1,2,4]; [9,9,9] + 1 = [1,0,0,0]
+        //* Example: [1,2,3] + 1 = [1,2,4]; [9,9,9] + 1 = [1,0,0,0]
         
         for(int i = digits.length - 1; i >= 0; i--){ // Traverse from right to left
             if(digits[i] < 9){ // No carry over needed
